7cbefcd22689cb0503429399f5d0d2a60509b49d
Heisenberg <esfahani.cyrus@gmail.com>
2024-08-27 19:01:20 -0700

    added info to our weatherRoutes.ts file

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,30 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+  const cityName = req.body.cityName;
+  WeatherService.getWeatherForCity(cityName).then(data => {
+    HistoryService.addCity(cityName);
+    res.json(data);
+  }
+  )
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  const cities = await HistoryService.getCities();
+  res.json(cities);
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  
+  res.json({ message: `City with id ${id} has been deleted` });
+});
 
 export default router;
